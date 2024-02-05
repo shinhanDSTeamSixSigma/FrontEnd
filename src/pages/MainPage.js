13694b14f81b998919b869965e284e85ebaa4570
kky <kky9616tt@gmail.com>
2024-02-06 05:25:44 +0900

    [feat] 초기세팅

---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import Header from '../layouts/Header';
+
+const MainPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="text-3xl">
+        <div>Main Page!</div>
+      </div>
+    </>
+  );
+};
+export default MainPage;
---
+++ src/router/root.js
@@ -0,0 +1,31 @@
+// 필요한 순간까지 컴포넌트를 메모리상으로 올리지 않도록 지연로딩
+import { Suspense, lazy } from 'react';
+
+const { createBrowserRouter } = require('react-router-dom');
+
+// 컴포넌트 처리 끝나기 전 로딩
+const Loading = <div>Loading....</div>;
+const Main = lazy(() => import('../pages/MainPage'));
+const Farm = lazy(() => import('../pages/FarmPage'));
+
+// 기본 라우팅 설정(어떤 경로에 어떤 컴포넌트 줄건지)
+const root = createBrowserRouter([
+    {
+        path: '',
+        element: (
+            <Suspense fallback={Loading}>
+                <Main />
+            </Suspense>
+        ),
+    },
+    {
+        path: 'farm',
+        element: (
+            <Suspense fallback={Loading}>
+                <Farm />
+            </Suspense>
+        ),
+    },
+]);
+
+export default root;
