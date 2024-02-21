920bcee8a0a4a5c6ade035b3895436747c778994
seyoung <young240956@gmail.com>
2024-02-21 20:45:18 +0900

    Feat:문의, 답변 문의 목록, 문의 상세, 답변 구현

--- src/components/FullButton.js
+++ src/components/FullButton.js
@@ -8,7 +8,7 @@ export default function FullButton(props) {
       <button
         type="button"
         style={buttonStyle}
-        class="rounded-md bg-[#80BCBD] px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-[#4F6F52]"
+        className="rounded-md bg-[#80BCBD] px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-[#4F6F52]"
       >
         {props.name}
       </button>
