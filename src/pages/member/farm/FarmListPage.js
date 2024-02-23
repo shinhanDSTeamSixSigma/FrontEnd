92da8668e2ab497e8187577fe02507007fa073cf
EricSamuelJun <mam675@naver.com>
2024-02-23 15:03:03 +0900

    Feat. 프론트 작업
    이미지 작업 처리 필요

--- src/pages/member/farm/FarmListPage.js
+++ src/pages/member/farm/FarmListPage.js
@@ -1,8 +1,7 @@
-
 import { Link } from 'react-router-dom';
 import FarmTypeAlignList from '../../../components/farm/FarmTypeAlign';
 import MyFarmList from '../../../components/farm/MyFarmList';
-const farms = [
+/*const farms = [
     {
         farm_no: 1,
         farm_name: '토심이네',
@@ -51,69 +50,23 @@ const farms = [
         farm_phone: '010-1111-1111',
         farm_orderNum: 10,
     },
-];
-function whenFarmListClicked(farmNo) {}
-function FarmObject(props) {
-    return (
-        <Link to={'/farm-detail'} key={props.farm.farm_no}>
-            <div className="d-flex shadow border rounded">
-                <div className="ml-3">
-                    <div>
-                        <h2 className="text-xl font-bold text-gray-900">
-                            {props.farm.farm_name}
-                        </h2>
-                    </div>
-                    <div className="d-flex bd-highlight mt-1">
-                        {/*<p className="text-sm text-gray-500">
-                        {props.farm.farm_rating}
-    </p>*/}
-                        <p className="text-lg">⭐</p>
-                        <p className="mt-1 font-bold">
-                            {props.farm.farm_rating}
-                        </p>{' '}
-                        <p className="mt-2 ml-1 text-sm">
-                            ({props.farm.farm_orderNum})
-                        </p>
-                        <p className="ml-2 mr-2 mt-1 text-sm">|</p>
-                        <p className="mt-1 text-sm">
-                            경력 {props.farm.farm_career}년
-                        </p>
-                        <p className="ml-2 mr-2 mt-1 text-sm">|</p>
-                        <p className="mt-1 text-sm">
-                            {props.farm.farm_address}
-                        </p>
-                    </div>
-                    <div>
-                        <p className="text-sm text-gray-500 mt-3">
-                            {props.farm.farm_content}
-                        </p>
-                    </div>
-                </div>
-                <img
-                    className="w-25 h-25 rounded flex-shrink-1 bd-highlight ms-auto shadows mt-2 mb-2 mr-2 ml-2"
-                    src={props.farm.image}
-                    alt=""
-                />
-            </div>
-        </Link>
-    );
-}
+];*/
 
 export default function FarmPage() {
     return (
         <>
-            {/* 카테고리별 가로 정렬 */}
-            <FarmTypeAlignList></FarmTypeAlignList>
+            {/* 카테고리별 가로 정렬 
+            <FarmTypeAlignList></FarmTypeAlignList>*/}
             {/* ㅁㅁ순 정렬*/}
 
-            {/* 리스트 */}
+            {/* 리스트 
             <ul className="">
                 {farms.map((farm) => (
                     <li key={farm.farm_no} className="py-4 w-100% ml-5 mr-5">
                         <FarmObject farm={farm} />
                     </li>
                 ))}
-            </ul>
+            </ul>*/}
             <ul className="divide-y divide-gray-200">
                 <MyFarmList />
             </ul>
