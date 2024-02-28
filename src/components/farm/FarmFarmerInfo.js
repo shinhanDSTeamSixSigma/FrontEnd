bdd831a69721fec36c2fad31691945a64f12aaeb
kky <kky9616tt@gmail.com>
2024-02-28 20:46:43 +0900

    Chore: build

--- src/components/farm/FarmFarmerInfo.js
+++ src/components/farm/FarmFarmerInfo.js
@@ -4,7 +4,7 @@ export default function FarmFarmerInfo({ farm, farmMember }) {
             <div className="my-6 font-semibold">{farm.farmName}</div>
             <div className="flex items-center mt-3 text-[#878787] ml-4 font-semibold text-base">
                 <img
-                    src={process.env.PUBLIC_URL + `/img/user.png`}
+                    src={process.env.PUBLIC_URL + `/img/User.png`}
                     alt=""
                     className="w-6 h-6  mr-2"
                 />
