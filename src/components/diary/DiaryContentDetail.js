2bd3a87dfb009e62f4ddc6afacece2a0f5d2b496
naayoung <ask5046a@gmail.com>
2024-02-28 14:24:25 +0900

    Style: 사진 css 수정

--- src/components/diary/DiaryContentDetail.js
+++ src/components/diary/DiaryContentDetail.js
@@ -25,7 +25,7 @@ const Picture = styled.img`
     background-color: #d9d9d9;
     border-radius: 0.8rem;
     margin-bottom: 1rem;
-    height: 8rem;
+    height: 20rem;
     width: 100%;
 `;
 const Height = styled.div`
